8e0b4071c3dc3ccb4c5c64288503e7041351bb70
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2019-01-27 01:34:22 -0200

    Update byteorder.c

--- byteorder.c
+++ byteorder.c
@@ -3,12 +3,12 @@
 int main(int argc, char **argv)
 {
 	union{
-		short  s;
-		char   c[sizeof(short)];
+		short s;
+		char  c[sizeof(short)];
 	}un;
 
 	un.s = 0x0102;
-	if (sizeof(short) == 2) {
+	if(sizeof(short) == 2){
 		if (un.c[0] == 1 && un.c[1] == 2){
 			printf("big-endian\n");
 			return(1);
